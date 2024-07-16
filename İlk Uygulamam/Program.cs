257dff7eee19cb2b4ac78cee06bc413e685aa088
FurkanCsk <furkancoskun99@hotmail.com>
2024-07-16 11:02:22 +0300

    Hello World!

---
+++ İlk Uygulamam/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Furkan Coşkun");
+Console.WriteLine("Patika'nın eğitimine ve eğitmenlerine güvendiğim için kariyer değişikliğimin ilk adımını burada atmaya karar verdim. Bu yüzden bu bootcampteyim!");
